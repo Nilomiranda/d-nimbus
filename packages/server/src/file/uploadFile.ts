29d772e9a729bba343da1f5edd2d01f06b051d0d
Nilomiranda <nilomiranda3@gmail.com>
2022-03-07 14:28:32 -0300

    chore: saving bucket file url

--- packages/server/src/file/uploadFile.ts
+++ packages/server/src/file/uploadFile.ts
@@ -1,23 +1,22 @@
-import {KoaContext} from "../globalInterfaces";
+import { KoaContext } from '../globalInterfaces'
 import { addHours } from 'date-fns'
 import Chance from 'chance'
 import { Storage } from '@google-cloud/storage'
 
 const storage = new Storage()
 
-const chance = new Chance();
+const chance = new Chance()
 
 export const uploadFile = async (context: KoaContext) => {
-  const { file, prisma, response } = context;
+  const { file, prisma, response } = context
 
   // size in bites
-  const { originalname, mimetype, buffer, size } = file;
-  const googleCloudFile = storage.bucket('d-number_bucket').file(`files/${originalname}`)
   const uuid = chance.guid({ version: 5 })
+  const { originalname, mimetype, buffer, size } = file
+  const googleCloudFile = storage.bucket('d-number_bucket').file(`files/${uuid}${originalname}`)
 
   try {
-    const uploadedFile = await googleCloudFile.save(buffer)
-    console.log(uploadedFile.)
+    await googleCloudFile.save(buffer)
 
     const savedFile = await prisma.file.create({
       data: {
@@ -25,26 +24,27 @@ export const uploadFile = async (context: KoaContext) => {
         expiresIn: addHours(new Date(), 24),
         uuid,
         mimeType: mimetype,
-        size
-      }
+        size,
+        url: `https://storage.googleapis.com/d-number_bucket/files/${uuid}${originalname}`,
+      },
     })
 
     if (savedFile) {
-      response.status = 200;
-      return response.body = {
-        ...savedFile
-      }
+      response.status = 200
+      return (response.body = {
+        ...savedFile,
+      })
     }
-    response.status = 500;
-    return response.body = {
-      error: 'File not saved.'
-    }
+    response.status = 500
+    return (response.body = {
+      error: 'File not saved.',
+    })
   } catch (err) {
     console.error('uploadFile:error::', err)
 
-    response.status = 500;
-    return response.body = {
-      error: 'File not saved.'
-    }
+    response.status = 500
+    return (response.body = {
+      error: 'File not saved.',
+    })
   }
 }
