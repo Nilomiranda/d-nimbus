8dfb583a4cc80479f1f70f14b5e18d64a92e8ae4
Nilomiranda <nilomiranda3@gmail.com>
2022-03-07 14:27:09 -0300

    chore: submitting files through the form

--- packages/client/pages/index.tsx
+++ packages/client/pages/index.tsx
@@ -1,4 +1,27 @@
+import {useRef, useState} from "react";
+import {uploadFile} from "../services/file";
+
 const Index = () => {
+  const [uploadedFile, setUploadedFile] = useState(null)
+
+  const fileRef = useRef(null)
+
+  const handleFileSubmit = async (event) => {
+    event.preventDefault();
+
+    const file = fileRef.current.files[0]
+
+    const formData = new FormData()
+    formData.append('document', file)
+
+    try {
+      const savedFile = await uploadFile(formData)
+      setUploadedFile(savedFile)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
   return (
     <div>
       <h1>Nimbus File Sharing</h1>
@@ -6,10 +29,27 @@ const Index = () => {
       <strong>Select a file to be uploaded and shared.</strong>
       <br /><br />
 
-      <label>
-        <span>Select file</span>
-        <input type="file" />
-      </label>
+      <form onSubmit={handleFileSubmit}>
+        <label>
+          <span>Select file</span>
+          <input type="file" ref={fileRef} />
+        </label>
+        <br/>
+
+        <button type="submit">Upload file</button>
+      </form>
+
+      <br />
+
+      {
+        uploadedFile ?
+          (
+            <div>
+              <p>File uploaded!</p>
+              <a href={`http://localhost:3000/file/${uploadedFile.id}`}>{uploadedFile.name}</a>
+            </div>
+          ) : null
+      }
     </div>
   )
 }
