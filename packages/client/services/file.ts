bd2f92da1d755bfcb36068b03cba764d582ab930
Nilomiranda <nilomiranda3@gmail.com>
2022-03-10 22:48:22 -0300

    chore: functions to read and send request to download file

--- packages/client/services/file.ts
+++ packages/client/services/file.ts
@@ -1,5 +1,8 @@
-import {httpClient} from "../config/httpClient";
-import { FileInterface } from "../interfaces/file";
+import { httpClient } from '../config/httpClient'
+import { FileInterface } from '../interfaces/file'
 
+export const uploadFile = async (payload: FormData): Promise<FileInterface> => (await httpClient.post('/upload', payload))?.data
 
-export const uploadFile = async (payload: FormData): Promise<FileInterface> => (await httpClient.post('/upload', payload))?.data;
+export const getFile = async (id: string): Promise<FileInterface> => (await httpClient.get(`/file/${id}`))?.data
+
+export const downloadFile = async (id: string) => httpClient.get(`/file/${id}/download`, { responseType: 'blob' })
