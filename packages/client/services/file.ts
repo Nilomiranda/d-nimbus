a42a75f4d2f92b7a1a59176d99e36d3ee24efb7b
Danilo <nilomiranda3@gmail.com>
2022-03-31 22:42:03 -0300

    chore: removed unused function

--- packages/client/services/file.ts
+++ packages/client/services/file.ts
@@ -3,6 +3,4 @@ import { FileInterface } from '../interfaces/file'
 
 export const uploadFile = async (payload: FormData): Promise<FileInterface> => (await httpClient.post('/upload', payload))?.data
 
-export const getFile = async (id: string): Promise<FileInterface> => (await httpClient.get(`/file/${id}`))?.data
-
 export const downloadFile = async (id: string) => httpClient.get(`/file/${id}/download`, { responseType: 'blob' })
